75c8c08de2a26b8e0998e953fa5c72dc2397e8a1
Daniel Mehler <danielmehler@Daniels-MacBook-Air.local>
2024-11-21 12:52:40 -0500

    Extract write_json() and write_csv() functions

--- isonewengland.py
+++ isonewengland.py
@@ -8,7 +8,30 @@ import urllib.request
 
 date_format = "%Y%m%d"
 
-def collect(start_date: str, end_date: str, username: str, password: str, output_dir: str = os.getcwd(), convert_to_csv: bool = False):
+def write_json(obj: dict, output_dir: str, filename_suffix: str):
+    with open(os.path.join(output_dir, f"iso-newengland-hourlylmp-{filename_suffix}.json"), 'w') as f:
+            f.write(obj)
+
+def write_csv(obj: dict, output_dir: str, filename_suffix: str):
+    # Convert JSON to CSV
+    headers = ["BeginDate", "LocationID", "LocationType", "LocationName", "LmpTotal", "EnergyComponent", "CongestionComponent", "LossComponent"]
+    with open(os.path.join(output_dir, f"iso-newengland-hourlylmp-{filename_suffix}.csv"), 'w', newline='\n') as f:
+        writer = csv.DictWriter(f, fieldnames=headers)
+        writer.writeheader()
+        # Write CSV
+        csv_obj = [{
+                "BeginDate": item["BeginDate"],
+                "LocationID": item["Location"]['@LocId'],
+                "LocationType": item["Location"]['@LocType'],
+                "LocationName": item["Location"]['$'],
+                "LmpTotal": item["LmpTotal"],
+                "EnergyComponent": item["EnergyComponent"],
+                "CongestionComponent": item["CongestionComponent"],
+                "LossComponent": item["LossComponent"]
+            } for item in obj["HourlyLmps"]["HourlyLmp"]]
+        writer.writerows(csv_obj)
+
+def collect(start_date: str, end_date: str, username: str, password: str, output_dir: str = os.getcwd(), aggregate_by: str = None, convert_to_csv: bool = False):
     # Create output folder
     os.makedirs(output_dir, exist_ok=True)
 
@@ -19,6 +42,7 @@ def collect(start_date: str, end_date: str, username: str, password: str, output
     # base64 encode username:password
     auth_bytes = base64.urlsafe_b64encode(bytes(f"{username}:{password}", 'utf-8'))
 
+    aggragation = {"HourlyLmps": {"HourlyLmp": []}}
     current = start
     # For each day
     while current <= end:
@@ -33,28 +57,9 @@ def collect(start_date: str, end_date: str, username: str, password: str, output
             with urllib.request.urlopen(req) as response:
                 resp = response.read().decode('utf-8')
             if not convert_to_csv:
-                with open(os.path.join(output_dir, f"iso-newengland-hourlylmp-{current.strftime(date_format)}-{hr}.json"), 'w') as f:
-                    f.write(resp)
+                write_json(obj=json.loads(resp), output_dir=output_dir, filename_suffix=f"{current.strftime(date_format)}-{hr}")
             else:
-                # Convert resp to JSON
-                json_obj = json.loads(resp)
-                # Convert JSON to CSV
-                headers = ["BeginDate", "LocationID", "LocationType", "LocationName", "LmpTotal", "EnergyComponent", "CongestionComponent", "LossComponent"]
-                with open(os.path.join(output_dir, f"iso-newengland-hourlylmp-{current.strftime(date_format)}-{hr}.csv"), 'w', newline='\n') as f:
-                    writer = csv.DictWriter(f, fieldnames=headers)
-                    writer.writeheader()
-                    # Write CSV
-                    csv_obj = [{
-                            "BeginDate": item["BeginDate"],
-                            "LocationID": item["Location"]['@LocId'],
-                            "LocationType": item["Location"]['@LocType'],
-                            "LocationName": item["Location"]['$'],
-                            "LmpTotal": item["LmpTotal"],
-                            "EnergyComponent": item["EnergyComponent"],
-                            "CongestionComponent": item["CongestionComponent"],
-                            "LossComponent": item["LossComponent"]
-                        } for item in json_obj["HourlyLmps"]["HourlyLmp"]]
-                    writer.writerows(csv_obj)
+                write_csv(obj=json.loads(resp), output_dir=output_dir, filename_suffix=f"{current.strftime(date_format)}-{hr}")
         # Increase "current" by 1 day
         current += timedelta(days=1)
                     
@@ -68,9 +73,10 @@ def main():
     parser.add_argument("--username", help="ISO New England username")
     parser.add_argument("--password", help="ISO New England password")
     parser.add_argument("--output-dir", help="Directory to output files")
+    parser.add_argument("--aggregate-by", default="hour", help="How to aggregate data. Default is \"hour\", meaning 1 file will be created for each hour of data. Other options are \"day\", \"month\", \"year\", and \"all\".")
     parser.add_argument("--csv", action="store_true", help="Save files as .CSV instead of .JSON")
     args = parser.parse_args()
-    collect(start_date=args.start_date, end_date=args.end_date, username=args.username, password=args.password, output_dir=args.output_dir, convert_to_csv=args.csv)
+    collect(start_date=args.start_date, end_date=args.end_date, username=args.username, password=args.password, output_dir=args.output_dir, aggregate_by=args.aggregate_by, convert_to_csv=args.csv)
 
 if __name__ == "__main__":
     main()
